8cd9e77684bc300a8a2374952b8d0929eb3bdbe7
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-10-12 01:12:52 +0530

    server responds to messages
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var WebSocketserver = require('ws').Server,
 ws = new WebSocketserver({port:8080})
 
-Connection: Upgrade;
-Upgrade: websocket;
-ws.on('connection', function(socket) {
-    console.log('clients connected')
-    socket.on('message', function() {
-        console.log(message)
-    })
+// Connection: Upgrade;
+// Upgrade: websocket;
+
+var clients = [];
+
+ws.on('connection', function(ws) {
+    var client_uuid = uuid.v4();
+    clients.push({"id": id, "ws": ws})
+    console.log("clients [%s] connected", clients_uuid) 
+})
+
+ws.on('message', function(message) {
+    for(var i = 0 ; i< clients.length; i++) {
+        var clientSocket = clients[i].ws
+        console.log("clients [%s]: %s", clients[i].id, message)
+        clientSocket.send(JSON.stringify({
+            'id': client_uuid,
+            'message': message,
+        }))
+    }
 })
