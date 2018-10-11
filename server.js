4865bae48a12a56e7696571da7e2164eafb979cf
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-10-12 02:17:57 +0530

    wsSend added with type as arg
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- server.js
+++ server.js
@@ -5,26 +5,47 @@ ws = new WebSocketserver({port:8080})
 // Upgrade: websocket;
 
 var clients = [];
+var clientIdx = 1;
 
-ws.on('connection', function(ws) {
-    var client_uuid = uuid.v4();
-    var nickname = client_uuid.substr(0,8)
-    clients.push({"id": id, "ws": ws, "nickname": nickname})
-    console.log("clients [%s] connected", clients_uuid) 
-})
-
-ws.on('message', function(message) {
+function wsSend(type,client_uuid,nickname,message) {
     for(var i = 0 ; i< clients.length; i++) {
         var clientSocket = clients[i].ws
         if(clientSocket.readyState == WebSocket.OPEN) {
             console.log("clients [%s]: %s", clients[i].id, message)
             clientSocket.send(JSON.stringify({
+                'type':type,
                 'id': client_uuid,
                 'message': message,
+                'nickname': nickname,
              }))
         }
     }
+}
+
+ws.on('connection', function(ws) {
+    var client_uuid = uuid.v4();
+    var nickname = "AnonymousUSER" + clientIdx
+    clientIdx +=1;
+    clients.push({"id": id, "ws": ws, "nickname": nickname})
+    var connect_message = nickname + 'has connected!'
+    wsSend('notification',client_uuid,nickname,connect_message)
 })
+
+ws.on('message', function(message) {
+    if(message.indexOf('/nick') == 0) {
+        var nickname_array = message.split(' ')
+        if(nickname_array.length > 2) {
+            var old_nickname = nickname;
+            nickname = nickname_array[1]
+            var nickname_message = "Client " + old_nickname + " changed to " + nickname
+            wsSend('nick_update',client_uuid,nickname,message)
+        } 
+    }else {
+        wsSend('message', client_uuid, nickname, message)
+    }
+})
+
+
 
 ws.on('close', function() {
     for(var i = 0;i< clients.length; i++) {
