3a96c1fe59c9207e54462e57a3e438658d7c5830
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-10-12 01:39:49 +0530

    send message func added
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/client.js
+++ src/client.js
@@ -2,3 +2,7 @@ var ws = new WebSocket('ws://localhost:8080')
 ws.onclose('open', function() {
     console.log('connection opened to the server')
 })
+
+function sendMessage(){
+    ws.send($('#message').val())
+}
